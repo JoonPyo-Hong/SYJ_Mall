971ef9a44bcd4bc2c075876759ac73db66bae6ac
Hong JUN PYO <wnsvy4231@naver.com>
2021-06-07 20:59:57 +0900

    복구!

--- SYJ_Mall/src/main/java/com/test/SYJ_Mall/MainController.java
+++ SYJ_Mall/src/main/java/com/test/SYJ_Mall/MainController.java
@@ -20,24 +20,20 @@ public class MainController {
 
 	@Autowired
 	private ILoginService logService;
-<<<<<<< HEAD
+
 	
 	
-	//처음에 로그인 페이지로 보내주는 곳 -> 광고와 함께 넘겨준다.
-	@RequestMapping(value = "login.action", method = { RequestMethod.GET })
-=======
+
 
 	// 처음에 로그인 페이지로 보내주는 곳
 	@RequestMapping(value = "/login.action", method = { RequestMethod.GET })
->>>>>>> HongJunpyo
 	public String login(HttpServletRequest request, HttpServletResponse response) {
 
 		// System.out.println("asd");
 		// 광고관련 넘겨야 한다. & 아이디 비밀번호 오류관련
 		Map<String, String> adverMap = logService.adverShow();
 		request.setAttribute("adverMap", adverMap);
-<<<<<<< HEAD
-		
+
 		
 		return "/login/UserLogin";
 	}
@@ -84,65 +80,14 @@ public class MainController {
 			
 	}
 	
-	//자동로그인 방지 체크
-	@RequestMapping(value = "/userautologinCheck.action", method = { RequestMethod.POST })
-	public void userAutologinCheck(HttpServletRequest request, HttpServletResponse response) throws Exception {
-			
-		PrintWriter out = response.getWriter();
-		JSONObject obj = logService.picCheck(request);
-		
-		out.print(obj);			
-=======
 
-		return "/login/UserLogin";
-	}
 
-	// 처음에 로그인 페이지로 보내주는 곳
-	@RequestMapping(value = "/loginVerification.action", method = { RequestMethod.POST })
-	public String loginVerification(HttpServletRequest request, HttpServletResponse response)
-			throws UnsupportedEncodingException {
-
-		request.setCharacterEncoding("UTF-8");
-
-		String id = (String) request.getParameter("id");// 아이디
-		String pw = (String) request.getParameter("pw");// 비밀번호
-
-		String ip = logService.ipCheck(request);
-		String encPw = logService.pwEnc(pw);// 상대방이 입력한 pw를 암호화작업해준다.
-
-		// String ip = logService.ipCheck(request);//ip check를 해준다.
-		// String encPw = logService.pwEnc(pw);//상대방이 입력한 pw를 암호화작업해준다.
-
-		int loginResult = logService.loginResult(ip, id, encPw);
-
-		if (loginResult == 0) {// 로그인 성공
-			System.out.println("로그인 성공");
-			return "/login/UserLogin";
-		} else if (loginResult == 1 || loginResult == -1) {// 로그인 실패 : 잘못된 로그인 정보 and 벤당한 아이피 들어오는경우
-			System.out.println("잘못된 로그인 정보");
-
-			// 아래에서 광고정보를 쇄신해준다. & 아이디 비밀번호 오류관련
-			Map<String, String> adverMap = logService.adverShow();
-			request.setAttribute("adverMap", adverMap);
-			request.setAttribute("loginError", 404);
-
-			return "/login/UserLogin";
-		} else {// 보안정책을 따라야하는 경우
-			System.out.println("보안정책을 따라야한다.");
-
-			// request = logService.AutoLoginBanned(request);
-			// 여기서 보안정책에 대해 성공한 경우는 또 로그인기록등을 남겨줘야 한다.
-
-			// return "autologin";
-			return "/login/UserLogin";
-		}
-	}
 	//사용자(구매자) 메인 화면
 	@RequestMapping(value = "/main.action", method = { RequestMethod.GET })
 	public String UserMain(HttpServletRequest request, HttpServletResponse response) {
 
 		return "/main/UserMain";
->>>>>>> HongJunpyo
+
 	}
 
 }
