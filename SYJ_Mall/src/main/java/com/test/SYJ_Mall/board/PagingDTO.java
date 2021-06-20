394c0cdbc3021626e986c27869ce7a2f6a711716
Hong JUN PYO <wnsvy4231@naver.com>
2021-06-20 21:36:59 +0900

    jsp 에서 오류 현재 오류 많이 고쳐짐

--- SYJ_Mall/src/main/java/com/test/SYJ_Mall/board/PagingDTO.java
+++ SYJ_Mall/src/main/java/com/test/SYJ_Mall/board/PagingDTO.java
@@ -4,7 +4,8 @@ public class PagingDTO {
 	// 현재페이지, 시작페이지, 끝페이지, 게시글 총 갯수, 페이지당 글 갯수, 마지막페이지, SQL쿼리에 쓸 start, end
 	private int nowPage, startPage, endPage, total, cntPerPage, lastPage, start, end;
 	private int cntPage = 5;
-
+	public PagingDTO() {
+	}
 	public PagingDTO(int total, int nowPage, int cntPerPage) {
 		setNowPage(nowPage);
 		setCntPerPage(cntPerPage);
