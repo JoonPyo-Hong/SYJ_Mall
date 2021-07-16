7044b21aceedcc0f6e16e56610523ede4ada7a83
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-07-16 23:26:41 +0900

    신규상품 컨트롤러 추가

---
+++ SYJ_Mall/src/main/java/com/test/SYJ_Mall/NewGoodsController.java
@@ -0,0 +1,14 @@
+package com.test.SYJ_Mall;
+
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * 신규상품 페이지 관련 컨트롤러
+ * @author shin
+ *
+ */
+@Controller
+public class NewGoodsController {
+
+}
