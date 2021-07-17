20d847d7729c65ed881c15e65f407507081f45d5
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-07-17 23:58:07 +0900

    신규테마 추가

--- SYJ_Mall/src/main/java/com/test/SYJ_Mall/NewGoodsController.java
+++ SYJ_Mall/src/main/java/com/test/SYJ_Mall/NewGoodsController.java
@@ -1,6 +1,12 @@
 package com.test.SYJ_Mall;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -10,5 +16,12 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class NewGoodsController {
-
+	
+	
+	// 처음에 로그인 페이지로 보내주는 곳
+	@RequestMapping(value = "/newGoodsStart.action", method = { RequestMethod.GET,RequestMethod.POST })
+	public String newGoodsStart(HttpServletRequest request, HttpServletResponse response, String site) {
+			
+		//return ;	
+	}
 }
