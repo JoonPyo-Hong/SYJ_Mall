c2ff5aa55ca045efbe1188d4604535de605a8e75
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-07-28 21:49:22 +0900

    kakaofriends - erd 설계 ver2.0.6

--- MSSQL/sh2.sql
+++ MSSQL/sh2.sql
@@ -101,3 +101,90 @@ CREATE TABLE [dbo].[KAKAO_PRODUCT_CATEGORY] (
 GO
 
 alter table dbo.KAKAO_PRODUCT_CATEGORY add constraint PK__KAKAO_PRODUCT_CATEGORY__CATEGORY_CODE__MAIN_CATEGORY_CODE primary key (category_code,main_category_code)
+
+
+
+
+------백업---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+/*
+	Author      : Seunghwan Shin
+	Create date : 2021-07-13
+	Description : 로그인 정보가 회원정보에 있는지 체크
+	    
+	History		: 2021-07-13 Seunghwan Shin	#최초 생성
+				  2021-07-23 Seunghwan Shin #마스터아이피(로컬아이피)는 몇번의 로그인 상관없이 ip벤 예외처리
+
+*/
+CREATE proc [dbo].[kakao_first_user_login]
+	@qoouser_id varchar(100) -- 유저 id
+,	@qoouser_pw varchar(800) -- 유저 pw
+,	@user_ip_address varchar(100)-- 접속한 ip주소
+,	@result int output -- 결과값 
+						/*	 0 : 벤당한 아이피로 접근
+							 1 : 로그인 정보가 존재함
+							-1 : 로그인 정보가 존재하지 않음 : 즉 아이디 비밀번호 둘중 하나이상이 틀렸다는 말임.
+							-100 : 오류발생
+						*/
+as
+set nocount on
+set transaction isolation level read uncommitted
+begin
+	begin try
+		-- 밴 당한 아이피인지 확인해준다.
+		if exists (select * from dbo.TBLBANNEDIPLIST where banned_ip_address = @user_ip_address) -- 벤당한 아이피의 경우
+		begin
+			set @result = 0
+		end
+		--벤 당하지 아닌 아이피인 경우 -> 로그인 정보 확인
+		else
+		begin
+			if exists (select * from dbo.QOO10_USER_REAL  where qoouser_id = @qoouser_id  and qoouser_pw = @qoouser_pw) -- 로그인 정보가 존재하는 경우
+			begin
+				set @result = 1
+			end
+			-- 정보가 존재하지 않는데 비정상적으로 계속 접근하는 경우
+			else
+			begin
+				set @result = -1
+
+				declare @try_count int
+
+				begin tran
+					--아이피 로그기록 남기기
+					insert into dbo.LOGINTRYIP
+					(
+						ip_address
+					,	try_time
+					)
+					values
+					(
+						@user_ip_address
+					,	default
+					)
+				commit tran
+
+				-- 시도한 횟수 : 15초안에 4번이상 시도하면, 밴을 시킨다.
+				select @try_count = count(*) from dbo.LOGINTRYIP with(nolock) 
+				where ip_address = @user_ip_address 
+				and  DATEDIFF(ss,try_time,getdate()) <= 15
+
+			    if (@try_count >= 4)
+				begin
+					if(@user_ip_address != '0:0:0:0:0:0:0:1')
+					begin
+						begin tran
+							insert into dbo.TBLBANNEDIPLIST values (@user_ip_address)
+						commit tran
+					end
+				end
+				
+			end
+		end
+	end try
+	begin catch
+		set @result = -100
+		rollback tran
+	end catch
+end
