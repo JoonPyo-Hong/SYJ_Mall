f9bc53366f3aa69dffa390d3d28f187d8684a2f9
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-03-15 22:33:55 +0900

    [기능] - 제품상세 페이지 알람기능 sp 추가

---
+++ MSSQL/Common/kakao_popular_product_alaram.sql
@@ -0,0 +1,75 @@
+﻿/* 
+	Author      : Seunghwan Shin 
+	Create date : 2021-10-11
+	Description : 상품 알람정보 관련
+	     
+	History	: 2021-10-11 Seunghwan Shin	#최초 생성
+			  2022-03-15 Seunghwan Shin	#트랜잭션 관련 로직 변경, 알람정보 수정될때 변경날짜 기록
+	Real DB : exec dbo.kakao_popular_product_alaram 2000001,13
+			  
+			  
+*/
+alter proc dbo.kakao_popular_product_alaram
+	@qoouser_seq	bigint		-- 회원 고유번호
+,	@product_id		bigint		-- 상품번호
+as
+set nocount on 
+set transaction isolation level read uncommitted 
+begin
+
+
+	if not exists (select * from dbo.KAKAO_USER_ALRAM_INFO with(nolock) where qoouser_seq = @qoouser_seq and product_id = @product_id)--알림정보에 상품이 없는경우 : 새로 추가
+	begin
+
+		insert into dbo.KAKAO_USER_ALRAM_INFO
+		(
+			qoouser_seq
+		,	product_id
+		,	reg_dt
+		,	chg_dt
+		,	del_yn
+		)
+		values
+		(
+			@qoouser_seq
+		,	@product_id
+		,	getdate()
+		,	null
+		,	'N'
+		)
+
+		if @@ERROR <> 0 return -1
+		else return 1
+	end
+	else if exists (select * from dbo.KAKAO_USER_ALRAM_INFO with(nolock) where qoouser_seq = @qoouser_seq and product_id = @product_id and del_yn = 'Y')--알림정보에 상품이 있는데 내가 알림끄기 설정한 경우 : 다시 켜기로 바꿔준다.
+	begin
+
+
+		update dbo.KAKAO_USER_ALRAM_INFO
+		set del_yn = 'N'
+		,	chg_dt = getdate()
+		where qoouser_seq = @qoouser_seq 
+		and product_id = @product_id
+
+		if @@ERROR <> 0 return -1
+		else return 1
+
+	end
+	else if exists (select * from dbo.KAKAO_USER_ALRAM_INFO with(nolock) where qoouser_seq = @qoouser_seq and product_id = @product_id and del_yn = 'N')--알림정보에 상품이 있는데 내가 알림켜기 설정한 경우 : 끄기로 바꿔준다.
+	begin
+			
+
+		update dbo.KAKAO_USER_ALRAM_INFO
+		set del_yn = 'Y'
+		,	chg_dt = getdate()
+		where qoouser_seq = @qoouser_seq 
+		and product_id = @product_id
+
+			
+		if @@ERROR <> 0 return -1
+		else return 2
+	end
+	
+
+end
+
