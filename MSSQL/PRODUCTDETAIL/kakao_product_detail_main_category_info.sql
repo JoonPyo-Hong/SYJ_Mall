d456441350dbc53714dd01a186514cbc1ed3782c
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-03-29 23:35:36 +0900

    [수정] - sp 최신화
    [수정] - sp 최신화

--- MSSQL/PRODUCTDETAIL/kakao_product_detail_main_category_info.sql
+++ MSSQL/PRODUCTDETAIL/kakao_product_detail_main_category_info.sql
@@ -1,21 +1,26 @@
 ﻿/* 
 	Author      : Seunghwan Shin 
-	Create date : 2022-03-27  
-	Description : 해당 상품 리뷰 전체 개수
+	Create date : 2021-03-29   
+	Description : 메인카테고리 정보 불러오기
 	     
-	History	: 2022-03-27 Seunghwan Shin	#최초생성
+	History	: 2021-03-29 Seunghwan Shin	#최초 생성	
+			  
 	
-	Real DB : exec dbo.kakao_product_detail_review_total_count 9
+	Real DB : exec dbo.kakao_product_detail_main_category_info
 
 */
-create proc dbo.kakao_product_detail_review_total_count
-	@prodt_seq		bigint		-- 상품고유 번호
+CREATE proc dbo.kakao_product_detail_main_category_info
 as 
 set nocount on 
 set transaction isolation level read uncommitted 
 begin
 	
-	select count(*) from dbo.KAKAO_PRODUCT_PAYMENT with(nolock)
-	where review_exists_yn = 'Y'
-			
+	select distinct 
+		 	kpc
+	.		main_category_code 
+	from dbo.KAKAO_PRODUCT_TABLE kpt with(nolock)
+	inner join dbo.KAKAO_PRODUCT_CATEGORY kpc with(nolock) on kpt.category_code = kpc.category_code
+	inner join dbo.KAKAO_PRODUCT_MAIN_CATEGORY kpmc with(nolock) on kpmc.main_category_code = kpc.main_category_code
+	
 end
+
