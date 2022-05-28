af1da567e8a67496000fd108f24ff2a17ef820b2
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-05-28 16:41:38 +0900

    [수정] 상품상세 리뷰총 개수 관련 sp 변경
    [수정] 상품상세 리뷰총 개수 관련 sp 변경

--- MSSQL/PRODUCTDETAIL/kakao_product_detail_review_total_count.sql
+++ MSSQL/PRODUCTDETAIL/kakao_product_detail_review_total_count.sql
@@ -4,11 +4,12 @@
 	Description : 해당 상품 리뷰 전체 개수
 	     
 	History	: 2022-03-27 Seunghwan Shin	#최초생성
+			  2022-05-28 Seunghwan Shin	#상품고우번호 필터링 조건 추가
 	
 	Real DB : exec dbo.kakao_product_detail_review_total_count 9
 
 */
-create proc dbo.kakao_product_detail_review_total_count
+alter proc dbo.kakao_product_detail_review_total_count
 	@prodt_seq		bigint		-- 상품고유 번호
 as 
 set nocount on 
@@ -17,5 +18,6 @@ begin
 	
 	select count(*) from dbo.KAKAO_PRODUCT_PAYMENT with(nolock)
 	where review_exists_yn = 'Y'
+	and product_id = @prodt_seq
 			
 end
